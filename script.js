73be700617cc631eb96a62d244311c95f1670cb1
mika <mikagivner123@gmail.com>
2023-02-02 23:06:27 +0200

    secondPush

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //bible:
 //0-sky
-//1-land
-//2-stone
+//1-dirt
+//2-rocks
 // 3-trunk
 //4-tree
+// 5-grass
+
+//tools:
+//Axe - for cutting trees גרזן
+//Pickaxe - for mining rocks מכוש
+//Shovel - for digging dirt את חפירה
 
 let gameBoard = document.querySelector(".gameBoard");
 let myPicture = [
@@ -19,12 +25,24 @@ let myPicture = [
   [0, 2, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 3, 3, 0, 0],
   [0, 2, 2, 0, 3, 3, 0, 0, 0, 0, 2, 0, 3, 3, 0, 2, 2, 0, 3, 3, 0, 0],
   [0, 2, 2, 2, 3, 3, 0, 0, 0, 2, 2, 0, 3, 3, 0, 2, 2, 0, 3, 3, 0, 0],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+let whichTool = 0;
+let axe = document.querySelector(".tool.axe");
+let pickaxe = document.querySelector(".tool.pickaxe");
+let shovel = document.querySelector(".tool.shovel");
+let rockAmount = 0;
+let rockImg = document.querySelector(".rockImg");
+let dirtAmount = 0;
+let dirtImg = document.querySelector(".dirtImg");
+let trunkAmount = 0;
+let trunkImg = document.querySelector(".trunkImg");
+let threeAmount = 0;
+let threeImg = document.querySelector(".threeImg");
 
 for (let i = 1; i < 17; i++) {
   for (let j = 1; j < 22; j++) {
@@ -36,14 +54,56 @@ for (let i = 1; i < 17; i++) {
     i--;
     j--;
     if (myPicture[i][j] === 0) newSquare.classList.add("sky");
-    else if (myPicture[i][j] === 1) newSquare.classList.add("land");
-    else if (myPicture[i][j] === 2) newSquare.classList.add("stone");
+    else if (myPicture[i][j] === 1) newSquare.classList.add("dirt");
+    else if (myPicture[i][j] === 2) newSquare.classList.add("rocks");
     else if (myPicture[i][j] === 3) newSquare.classList.add("trunk");
+    else if (myPicture[i][j] === 5) newSquare.classList.add("grassDirt");
     else newSquare.classList.add("tree");
   }
 }
 
 gameBoard.addEventListener("click", function (e) {
-  console.log(e.target);
-  e.target.setAttribute("class", "sky");
+  if (
+    (whichTool === 1 && e.target.className === "trunk") ||
+    (whichTool === 1 && e.target.className === "tree")
+  ) {
+    if (e.target.className !== "gameBoard") {
+      if (e.target.className === "trunk") {
+        trunkAmount++;
+        trunkImg.innerText = `${trunkAmount.toString()}`;
+      } else {
+        threeAmount++;
+        threeImg.innerText = `${threeAmount.toString()}`;
+      }
+      e.target.setAttribute("class", "sky");
+    }
+  } else if (whichTool === 2 && e.target.className === "rocks") {
+    if (e.target.className !== "gameBoard") {
+      e.target.setAttribute("class", "sky");
+      rockAmount++;
+      rockImg.innerText = `${rockAmount.toString()}`;
+    }
+  } else if (
+    (whichTool === 3 && e.target.className === "dirt") ||
+    (whichTool === 3 && e.target.className === "grassDirt")
+  ) {
+    if (e.target.className !== "gameBoard") {
+      e.target.setAttribute("class", "sky");
+      dirtAmount++;
+      dirtImg.innerText = `${dirtAmount.toString()}`;
+    }
+  }
+});
+
+axe.addEventListener("click", function () {
+  whichTool = 1;
+  console.log(whichTool);
+});
+pickaxe.addEventListener("click", function () {
+  whichTool = 2;
+  console.log(whichTool);
+});
+shovel.addEventListener("click", function () {
+  whichTool = 3;
+  console.log(whichTool);
 });
